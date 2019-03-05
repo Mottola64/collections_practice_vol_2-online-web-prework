3514207e24074c25f5a361e0409a3a95bd712dd7
Michael Mottola <mottola64@gmail.com>
2019-03-05 03:07:24 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,2 +1,13 @@
-def organize_schools(schools)
-  organized
+def organized_schools(schools)
+  organized_schools = {}
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+      else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
+end
